1126c7b9d75b947640b278ab41c4b44af19daacf
Robert Larkins <ayb4btu@gmail.com>
2021-07-08 21:39:33 +1200

    Add RomanNumeral conversion
    This is the first iteration of the roman numeral conversion.
    All test cases pass, but the code needs refactoring to improve its
    quality.

---
+++ Larkins.CSharpKatas/src/Larkins.CSharpKatas/RomanNumeral.cs
@@ -0,0 +1,254 @@
+﻿using CSharpFunctionalExtensions;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Larkins.CSharpKatas
+{
+    public class RomanNumeral
+    {
+        public RomanNumeral(int arabicNumeral, string romanNumeral)
+        {
+            IntValue = arabicNumeral;
+            Value = romanNumeral;
+        }
+
+        public int IntValue { get; }
+
+        public string Value { get; }
+
+        public static Result<RomanNumeral> Create(string romanNumeral)
+        {
+            if (!IsValid(romanNumeral))
+            {
+                return Result.Failure<RomanNumeral>($"'{romanNumeral}' is an invalid Roman Numeral");
+            }
+
+            var intValue = 0;
+
+            for (var i = 0; i < romanNumeral.Length; i++)
+            {
+                var currentCharacterResult = RomanNumeralCharacter.Create(romanNumeral[i]);
+
+                if (currentCharacterResult.IsFailure)
+                {
+                    return Result.Failure<RomanNumeral>(currentCharacterResult.Error);
+                }
+
+                var currentValue = currentCharacterResult.Value;
+                RomanNumeralCharacter nextCharacter = null;
+                if (i < romanNumeral.Length - 1)
+                {
+                    var nextCharacterResult = RomanNumeralCharacter.Create(romanNumeral[i + 1]);
+
+                    if (nextCharacterResult.IsFailure)
+                    {
+                        return Result.Failure<RomanNumeral>(nextCharacterResult.Error);
+                    }
+
+                    nextCharacter = nextCharacterResult.Value;
+                }
+
+                if (currentValue.IsSubtractable && currentValue.Value < nextCharacter?.Value)
+                {
+                    intValue -= currentValue.Value;
+                    intValue += nextCharacter.Value;
+                    i++;
+                }
+                else
+                {
+                    intValue += currentValue.Value;
+                }
+            }
+
+            return new RomanNumeral(intValue, romanNumeral);
+
+            static bool IsValid(string romanNumeral)
+            {
+                var romanNumeralPattern = "^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$";
+                var regex = new Regex(romanNumeralPattern);
+
+                return regex.IsMatch(romanNumeral);
+            }
+        }
+
+        public static Result<RomanNumeral> Create(int arabicNumeral)
+        {
+            if (arabicNumeral is <= 0 or >= 4000)
+            {
+                return Result.Failure<RomanNumeral>($"Arabic numeral {arabicNumeral} must be between 1 and 3999.");
+            }
+
+            var rm = "";
+
+            var divider = 1000;
+
+            while (divider > 0)
+            {
+                var digit = arabicNumeral / divider;
+                arabicNumeral %= divider;
+
+                if (divider == 1000 && digit > 0)
+                {
+                    for (var i = 0; i < digit; i++)
+                    {
+                        rm += "M";
+                    }
+                }
+                else if (divider == 100)
+                {
+                    if (digit == 9)
+                    {
+                        rm += "CM";
+                    }
+                    else if (digit >= 5)
+                    {
+                        rm += "D";
+
+                        for (var i = 0; i < digit - 5; i++)
+                        {
+                            rm += "C";
+                        }
+                    }
+                    else if (digit == 4)
+                    {
+                        rm += "CD";
+                    }
+                    else
+                    {
+                        for (var i = 0; i < digit; i++)
+                        {
+                            rm += "C";
+                        }
+                    }
+                }
+                else if (divider == 10)
+                {
+                    if (digit == 9)
+                    {
+                        rm += "XC";
+                    }
+                    else if (digit >= 5)
+                    {
+                        rm += "L";
+
+                        for (var i = 0; i < digit - 5; i++)
+                        {
+                            rm += "X";
+                        }
+                    }
+                    else if (digit == 4)
+                    {
+                        rm += "XL";
+                    }
+                    else
+                    {
+                        for (var i = 0; i < digit; i++)
+                        {
+                            rm += "X";
+                        }
+                    }
+                }
+                else if (divider == 1)
+                {
+                    if (digit == 9)
+                    {
+                        rm += "IX";
+                    }
+                    else if (digit >= 5)
+                    {
+                        rm += "V";
+
+                        for (var i = 0; i < digit - 5; i++)
+                        {
+                            rm += "I";
+                        }
+                    }
+                    else if (digit == 4)
+                    {
+                        rm += "IV";
+                    }
+                    else
+                    {
+                        for (var i = 0; i < digit; i++)
+                        {
+                            rm += "I";
+                        }
+                    }
+                }
+
+                divider /= 10;
+            }
+
+            return new RomanNumeral(arabicNumeral, rm);
+        }
+
+        private class RomanNumeralCharacter
+        {
+            private RomanNumeralCharacter(char character, int value)
+            {
+                Character = character;
+                Value = value;
+            }
+
+            public char Character { get; }
+
+            public int Value { get; }
+
+            public bool IsSubtractable => new List<int> { 1, 10, 100 }.Contains(Value);
+
+        public static Result<RomanNumeralCharacter> Create(char character)
+            {
+                var valueResult = RomanNumeralValueLookUp(character);
+
+                if (valueResult.IsFailure)
+                {
+                    return Result.Failure<RomanNumeralCharacter>(valueResult.Error);
+                }
+
+                return new RomanNumeralCharacter(character, valueResult.Value);
+            }
+
+            public static Result<RomanNumeralCharacter> Create(int value)
+            {
+                var charResult = RomanNumeralCharacterLookUp(value);
+
+                if (charResult.IsFailure)
+                {
+                    return Result.Failure<RomanNumeralCharacter>(charResult.Error);
+                }
+
+                return new RomanNumeralCharacter(charResult.Value, value);
+            }
+
+            private static Result<int> RomanNumeralValueLookUp(char character)
+            {
+                return character switch
+                {
+                    'I' => 1,
+                    'V' => 5,
+                    'X' => 10,
+                    'L' => 50,
+                    'C' => 100,
+                    'D' => 500,
+                    'M' => 1000,
+                    _ => Result.Failure<int>("Unrecognised character.")
+                };
+            }
+
+            private static Result<char> RomanNumeralCharacterLookUp(int value)
+            {
+                return value switch
+                {
+                    1 => 'I',
+                    5 => 'V',
+                    10 => 'X',
+                    50 => 'L',
+                    100 => 'C',
+                    500 => 'D',
+                    1000 => 'M',
+                    _ => Result.Failure<char>("No character associated with this value.")
+                };
+            }
+        }
+    }
+}
---
+++ Larkins.CSharpKatas/tests/Larkins.CSharpKatas.Tests.Unit/RomanNumeralTests.cs
@@ -0,0 +1,121 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Larkins.CSharpKatas.Tests.Unit
+{
+    public class RomanNumeralTests
+    {
+        [Theory]
+        [InlineData("I", 1)]
+        [InlineData("II", 2)]
+        [InlineData("III", 3)]
+        [InlineData("IV", 4)]
+        [InlineData("V", 5)]
+        [InlineData("VI", 6)]
+        [InlineData("VII", 7)]
+        [InlineData("VIII", 8)]
+        [InlineData("IX", 9)]
+        [InlineData("X", 10)]
+        [InlineData("XVI", 16)]
+        [InlineData("XXVII", 27)]
+        [InlineData("XXXII", 32)]
+        [InlineData("LVIII", 58)]
+        [InlineData("CLXXXIII", 183)]
+        [InlineData("DLV", 555)]
+        [InlineData("XLV", 45)]
+        [InlineData("L", 50)]
+        [InlineData("XCVIII", 98)]
+        [InlineData("XCIX", 99)]
+        [InlineData("C", 100)]
+        [InlineData("CD", 400)]
+        [InlineData("D", 500)]
+        [InlineData("CMXCIV", 994)]
+        [InlineData("CM", 900)]
+        [InlineData("CMXCIX", 999)]
+        [InlineData("M", 1000)]
+        [InlineData("MMMCMXCIX", 3999)]
+        public void Roman_numeral_converts_to_associated_number(string romanNumeral, int expected)
+        {
+            // Arrange
+            var sut = RomanNumeral.Create(romanNumeral).Value;
+
+            // Act
+            var result = sut.IntValue;
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("ABCD")]
+        [InlineData("IIII")]
+        [InlineData("VV")]
+        [InlineData("VIV")]
+        [InlineData("   ")]
+        [InlineData("MMMDD")]
+        [InlineData("MMMM")]
+        [InlineData("IXC")]
+        [InlineData("IXCM")]
+        [InlineData("XCM")]
+        public void Unknown_roman_numeral_is_invalid(string romanNumeral)
+        {
+            var result = RomanNumeral.Create(romanNumeral);
+
+            result.IsFailure.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(-10)]
+        [InlineData(0)]
+        [InlineData(4000)]
+        [InlineData(12345)]
+        public void Number_outside_roman_numeral_range_is_invalid(int number)
+        {
+            var result = RomanNumeral.Create(number);
+
+            result.IsFailure.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(1, "I")]
+        [InlineData(2, "II")]
+        [InlineData(3, "III")]
+        [InlineData(4, "IV")]
+        [InlineData(5, "V")]
+        [InlineData(6, "VI")]
+        [InlineData(7, "VII")]
+        [InlineData(8, "VIII")]
+        [InlineData(9, "IX")]
+        [InlineData(10, "X")]
+        [InlineData(16, "XVI")]
+        [InlineData(27, "XXVII")]
+        [InlineData(32, "XXXII")]
+        [InlineData(58, "LVIII")]
+        [InlineData(183, "CLXXXIII")]
+        [InlineData(555, "DLV")]
+        [InlineData(45, "XLV")]
+        [InlineData(50, "L")]
+        [InlineData(98, "XCVIII")]
+        [InlineData(99, "XCIX")]
+        [InlineData(100, "C")]
+        [InlineData(400, "CD")]
+        [InlineData(500, "D")]
+        [InlineData(994, "CMXCIV")]
+        [InlineData(900, "CM")]
+        [InlineData(999, "CMXCIX")]
+        [InlineData(1000, "M")]
+        [InlineData(3999, "MMMCMXCIX")]
+        public void Number_converts_to_roman_numeral(int value, string expected)
+        {
+            // Arrange
+            var sut = RomanNumeral.Create(value).Value;
+
+            // Act
+            var result = sut.Value;
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
