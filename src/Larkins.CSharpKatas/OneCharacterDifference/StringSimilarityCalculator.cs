e0e1a6740c14528badfe5a0b4781ca05bc5c7f14
Robert Larkins <ayb4btu@gmail.com>
2025-08-25 21:30:51 +1200

    Fix string similarity calculator
    This improves the tests to identify bugs in the algorithm.

--- src/Larkins.CSharpKatas/OneCharacterDifference/StringSimilarityCalculator.cs
+++ src/Larkins.CSharpKatas/OneCharacterDifference/StringSimilarityCalculator.cs
@@ -15,8 +15,6 @@ public static class StringSimilarityCalculator
 
         var differenceCount = 0;
 
-        // if length difference = 1
-        // abc, abcd
         for (int i = 0, j = 0; i < input1.Length && j < input2.Length; i++, j++)
         {
             if (input1[i] == input2[j])
@@ -31,18 +29,23 @@ public static class StringSimilarityCalculator
                 return false;
             }
 
+            if (lengthDifference == 0)
+            {
+                continue;
+            }
+
             // Continue checking from the current character in the shorter string
             // Ignore the current character in the longer string as it doesn't match.
-            if (input1.Length > input2.Length && lengthDifference == 1)
-            {
-                j--;
-            }
-            else
+            if (input1.Length < input2.Length)
             {
                 i--;
             }
+            else
+            {
+                j--;
+            }
         }
 
-        return true;
+        return differenceCount == 1 || differenceCount == 0 && lengthDifference == 1;
     }
 }
